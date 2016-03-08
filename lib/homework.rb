ac06044d97039b7144eaaa91e8916e7927d915e0
Brit Butler <redline6561@gmail.com>
2016-03-08 12:00:07 -0500

    Add basic github interface.

--- lib/homework.rb
+++ lib/homework.rb
@@ -1,5 +1,13 @@
+require "pry"
+require "httparty"
+
 require "homework/version"
+require "homework/github"
 
 module Homework
   # Your code goes here...
 end
+
+github = Homework::Github.new
+
+binding.pry
---
+++ lib/homework/github.rb
@@ -0,0 +1,32 @@
+module Homework
+  class Github
+    include HTTParty
+    base_uri "https://api.github.com"
+
+    def initialize
+      @auth_token = "a8ba164f9526540607f9a91b0625120ff5352f3e"
+      @headers = {
+        "Authorization" => "token #{@auth_token}",
+        "User-Agent"    => "HTTParty"
+      }
+    end
+
+    def get_user(username)
+      Github.get("/users/#{username}", headers: @headers)
+    end
+
+    def list_members_by_team_name(org, team_name)
+      teams = list_teams(org)
+      team = teams.find { |team| team["name"] == team_name }
+      list_team_members(team["id"])
+    end
+
+    def list_teams(organization)
+      Github.get("/orgs/#{organization}/teams", headers: @headers)
+    end
+
+    def list_team_members(team_id)
+      Github.get("/teams/#{team_id}/members", headers: @headers)
+    end
+  end
+end
